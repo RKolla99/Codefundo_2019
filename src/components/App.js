0ed89ccc3a86647a30db6dd01e4a91156eec9687
Ishar-Menon <ishar1999messi@gmail.com>
2019-08-17 09:18:36 +0530

    updated constituency page to write to the blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Web3 from "web3";
 import TruffleContract from "truffle-contract";
 import IndianElection from "../../build/contracts/IndianElection.json";
 import AdminLogin from "../pages/adminLogin";
-import Constitution from "../pages/constitution";
+import Constituency from "../pages/constituency";
 import Home from "../pages/home";
 import Thankyou from "../pages/thankyou";
 import VoterLogin from "../pages/login";
@@ -18,7 +18,7 @@ class App extends Component {
       hasVoted: false,
       loading: true,
       voting: false,
-      pageIndex: 5
+      pageIndex: 1
     };
     if (typeof web3 != "undefined") {
       this.web3Provider = web3.currentProvider;
@@ -32,49 +32,25 @@ class App extends Component {
     this.election = TruffleContract(IndianElection);
     this.election.setProvider(this.web3Provider);
   }
-  //   componentWillMount() {
-  //     this.loadBlockchainData();
-  //   }
 
-  //   async loadBlockchainData() {
-  //     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
-  //     const accounts = await web3.eth.getAccounts();
-  //     alert(accounts[0]);
-  //   }
   componentDidMount() {
     this.web3.eth.getCoinbase((err, account) => {
       this.setState({ account });
-      this.election.deployed().then(electionInstance => {
-        this.electionInstance = electionInstance;
-        // this.watchEvents();
-        this.electionInstance.candidatesInformation(1, 0).then(candidate => {
-          alert(candidate);
-          const candidates = [...this.state.candidates];
-          candidates.push({
-            id: candidate[0],
-            name: candidate[1],
-            voteCount: candidate[2]
-          });
-
-          this.setState({ candidates: candidates });
-          this.setState({ hasVoted: false, loading: false });
-        });
-      });
     });
   }
   render() {
     if (this.state.pageIndex == 0) {
-      return <AdminLogin />;
+      return <AdminLogin mycon={this.election} account={this.state.account} />;
     } else if (this.state.pageIndex == 1) {
-      return <Constitution mycon={this.election} />;
+      return <Constituency mycon={this.election} myweb3={this.web3} />;
     } else if (this.state.pageIndex == 2) {
-      return <VoterLogin />;
+      return <VoterLogin mycon={this.election} account={this.state.account} />;
     } else if (this.state.pageIndex == 3) {
-      return <Vote />;
+      return <Vote mycon={this.election} account={this.state.account} />;
     } else if (this.state.pageIndex == 4) {
-      return <Home />;
+      return <Home mycon={this.election} />;
     } else {
-      return <Thankyou />;
+      return <Thankyou mycon={this.election} />;
     }
   }
 }
---
+++ src/pages/constituency.js
@@ -0,0 +1,271 @@
+import React from "react";
+import AddCandidate from "../components/addCandidate";
+import Web3 from "web3";
+import {
+  Button,
+  Label,
+  Alert,
+  Modal,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  Table
+} from "reactstrap";
+// import {Link} from 'react-router-dom';
+
+const constitutionStyle = {
+  width: "60%",
+  margin: "10px auto 5px auto",
+  padding: "10px 10px 10px 10px",
+  // border: '1px black solid',
+  boxShadow: "0px 5px 10px 2px #000"
+};
+
+const labelStyle = {
+  display: "inline-block",
+  width: "180px",
+  padding: "5px"
+};
+
+const alertStyle = {
+  width: "80%",
+  margin: "10px auto 15px auto"
+};
+
+const buttonsStyle = {
+  // width: '40%',
+  margin: "10px auto 5px auto",
+  overflow: "auto"
+};
+
+class Constituency extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCandidateDetails: false,
+      showConstitutionDetails: true,
+      addCandidate: false,
+      candidates: Object(),
+      constitution: Object(),
+      submit: false,
+      confirmSubmit: false,
+      web3: this.props.myweb3,
+      account: "0x0"
+      // blockNumber: 0
+    };
+    this.addConstituency = this.addConstituency.bind(this);
+    this.addCandidate = this.addCandidate.bind(this);
+    this.submit = this.submit.bind(this);
+    this.confirmSubmit = this.confirmSubmit.bind(this);
+    this.confirmSubmitToggle = this.confirmSubmitToggle.bind(this);
+    this.election = this.props.mycon;
+    // this.watchEvents = this.watchEvents.bind(this);
+  }
+
+  componentDidMount() {
+    this.state.web3.eth.getCoinbase((err, account) => {
+      this.setState({ account });
+      this.election.deployed().then(electionInstance => {
+        this.electionInstance = electionInstance;
+      });
+    });
+  }
+
+  addConstituency() {
+    const const_name = document.getElementById("const_name").value;
+    const const_id = parseInt(document.getElementById("const_id").value, 10);
+
+    this.electionInstance
+      .addConstituency(const_id, const_name, 0, { from: this.state.account })
+      .then(result => {
+        const newConstitution = { const_id, const_name };
+        this.setState({
+          showCandidateDetails: true,
+          showConstitutionDetails: false,
+          addCandidate: false,
+          constitution: newConstitution
+        });
+      });
+  }
+
+  addCandidate() {
+    if (this.state.addCandidate) {
+      const cand_name = document.getElementById("cand_name").value;
+      const cand_id = document.getElementById("cand_id").value;
+
+      const candidate = {
+        [cand_id]: cand_name
+      };
+
+      this.setState({
+        candidates: { ...this.state.candidates, ...candidate }
+      });
+
+      document.getElementById("cand_name").value = "";
+      document.getElementById("cand_id").value = "";
+    }
+
+    this.setState({
+      addCandidate: true
+    });
+  }
+
+  async submit() {
+    alert(this.state.constitution.const_id);
+    this.confirmSubmitToggle();
+    var size = Object.keys(this.state.candidates).length;
+    var i;
+    for (i = 1; i <= size; i++) {
+      await this.electionInstance.addCandidate(
+        this.state.constitution.const_id,
+        i - 1,
+        this.state.candidates[i],
+        { from: this.state.account }
+      );
+    }
+    this.setState({
+      submit: true
+    });
+  }
+
+  confirmSubmit() {
+    this.setState({
+      confirmSubmit: true
+    });
+  }
+
+  confirmSubmitToggle() {
+    this.setState({
+      confirmSubmit: false
+    });
+  }
+
+  render() {
+    if (this.state.addCandidate) {
+      var candidates_arr = [];
+      var count = 1;
+      for (var i in this.state.candidates) {
+        candidates_arr.push(
+          <tr key={i + 1}>
+            <td>{count}</td>
+            <td className="text-center">{this.state.candidates[i]}</td>
+          </tr>
+        );
+        count++;
+      }
+    }
+
+    return (
+      <div>
+        <h1>{this.state.account}</h1>
+        {this.state.showConstitutionDetails && (
+          <div id="constitution_details" style={constitutionStyle}>
+            <Label for="const_name" style={labelStyle}>
+              Constituency Name
+            </Label>
+            <input id="const_name" />
+            <br />
+            <Label for="const_id" style={labelStyle}>
+              Constituency ID
+            </Label>
+            <input id="const_id" />
+            <br />
+            <div className="text-right">
+              <Button size="sm" onClick={this.addConstituency}>
+                NEXT
+              </Button>
+            </div>
+          </div>
+        )}
+        {!this.state.showConstitutionDetails && (
+          <div className="text-center">
+            <Alert color="success" style={alertStyle}>
+              Constituency successfully added!
+            </Alert>
+          </div>
+        )}
+        <div style={{ overflow: "auto" }}>
+          <div className="float-left" style={{ width: "50%" }}>
+            {this.state.addCandidate && (
+              <div>
+                <AddCandidate />
+              </div>
+            )}
+            {this.state.showCandidateDetails && (
+              <div className="text-right" style={buttonsStyle}>
+                <div className="text-center">
+                  <Button className="primary" onClick={this.addCandidate}>
+                    Add Candidate
+                  </Button>
+                </div>
+              </div>
+            )}
+          </div>
+          {this.state.addCandidate &&
+            Object.keys(this.state.candidates).length > 0 && (
+              <div className="float-right" style={{ width: "50%" }}>
+                <div style={{ width: "60%", margin: "auto" }}>
+                  <Table hover>
+                    <thead>
+                      <tr>
+                        <td>Sl No.</td>
+                        <td>Candidate Name</td>
+                      </tr>
+                    </thead>
+                    <tbody>{candidates_arr}</tbody>
+                  </Table>
+                </div>
+                <div
+                  style={{ width: "60%", margin: "auto" }}
+                  className="text-center"
+                >
+                  <Button className="btn-success" onClick={this.confirmSubmit}>
+                    Submit
+                  </Button>
+                </div>
+                <div>
+                  <Modal
+                    isOpen={this.state.confirmSubmit}
+                    toggle={this.confirmSubmitToggle}
+                  >
+                    <ModalHeader toggle={this.confirmSubmitToggle}>
+                      Confirm to save candidate details
+                    </ModalHeader>
+                    <ModalBody>
+                      Are you sure you want to add no more candidates?
+                    </ModalBody>
+                    <ModalFooter>
+                      <Button
+                        className="btn-success"
+                        size="sm"
+                        onClick={this.submit}
+                      >
+                        Submit
+                      </Button>
+                      <Button
+                        className="btn-danger"
+                        size="sm"
+                        onClick={this.confirmSubmitToggle}
+                      >
+                        Cancel
+                      </Button>
+                    </ModalFooter>
+                  </Modal>
+                </div>
+              </div>
+            )}
+          {this.state.submit && (
+            <div>
+              <Modal isOpen={true}>
+                <ModalHeader>Details Saved Successfully!</ModalHeader>
+                <ModalFooter>Return to home</ModalFooter>
+              </Modal>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Constituency;
